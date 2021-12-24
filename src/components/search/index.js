6614e2b88cdf08f89e3dbfb88bcd1974264dadcc
ChekirovDaniyar <arturo.gatti4040@gmail.com>
2021-12-24 16:11:53 +0600

    Fix year

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -131,6 +131,7 @@ const Search = () => {
                 nextMonth={nextMonth}
                 previousMonth={previousMonth}
                 setRef={setRef}
+                isNavigatePossible={isNavigatePossible}
               />
             )}
           </div>
